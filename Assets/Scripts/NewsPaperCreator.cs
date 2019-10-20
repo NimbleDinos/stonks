728fd8628cc8d0ac9b79ac47246cbffebe13a23a
JJMaslen <JordanDmaslen@gmail.com>
2019-10-20 08:11:31 +0100

    Newspaper Things

---
+++ Assets/Scripts/NewsPaperCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsPaperCreator : MonoBehaviour
+{
+    public GameObject Paper;
+    int timeCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCheck = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timeCheck != Time_Handler.currentHour)
+        {
+            Instantiate(Paper);
+            timeCheck = Time_Handler.currentHour;
+        }
+    }
+}
